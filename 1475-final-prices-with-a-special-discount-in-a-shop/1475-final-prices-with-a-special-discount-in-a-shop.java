694bd48ff0622bc972882e36510cacd0ab5ede89
Lokeshjampana-cse <vtu29567@veltech.edu.in>
2026-08-25 14:23:41 +0530

    Time: 4 ms (50.23%), Space: 45.1 MB (86.73%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] answer = prices.clone();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+
+            // Current price can be the discount for previous items
+            while (!stack.isEmpty() &&
+                   prices[i] <= prices[stack.peek()]) {
+
+                int index = stack.pop();
+                answer[index] = prices[index] - prices[i];
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
